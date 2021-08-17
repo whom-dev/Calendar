4b4f33c4aa2f0df95bd8b1ee905ee50485e5e121
bollhagen7 <bollhagen7@gmail.com>
2021-08-17 11:14:59 -0500

    Add project files.

---
+++ Data/EFTestDbRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calendar.Data
+{
+    public class EFTestDbRepository : ITestDbRepository
+    {
+        public TestDbContext context;
+
+        public EFTestDbRepository(TestDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        //public IQueryable<model> model=>context.model;
+    }
+}
---
+++ Data/TestDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Calendar.Data
+{
+    public class TestDbContext : DbContext
+    {
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options) { }
+
+        //public DbSet<model> model {get;set;}
+    }
+}

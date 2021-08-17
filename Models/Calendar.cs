3b8b01f89b735517976dc8b8e455998266069a32
bollhagen7 <bollhagen7@gmail.com>
2021-08-17 13:20:06 -0500

    DB setup, Calendar Model

---
+++ Models/Calendar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calendar.Models
+{
+    public class Calendar
+    {
+        public int CalendarID { get; set; }
+
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string ThemeColor { get; set; }
+        public bool IsFullDay { get; set; }
+    }
+}
